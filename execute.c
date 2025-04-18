2dd9b1e091de57eb93f8320e16751aaecdeca549
Your Name <bezom@hotmail.fr>
2025-04-18 23:07:02 +0200

    modification fichier execute.c

--- execute.c
+++ execute.c
@@ -1,10 +1,37 @@
 #include "shell.h"
 
 /**
- * execute_command - Exécute une commande.
+ * execute_builtin - Executes a built-in command.
+ * @args: An array of strings containing the command
+ * and its arguments.
  *
- * Args :
- *   args : Un tableau de chaînes contenant la commande et ses arguments.
+ * Return: 1 if the command is a built-in and was executed,
+ * 0 otherwise.
+ */
+int execute_builtin(char **args) {
+    if (args == NULL || args[0] == NULL) {
+        return 0;
+    }
+
+    if (strcmp(args[0], "env") == 0) {
+        
+        extern char **environ;
+        char **env = environ;
+        while (*env != NULL) {
+            printf("%s\n", *env);
+            env++;
+        }
+        return (1);
+    }
+
+    return (0);
+}
+
+/**
+ * execute_command - Executes a command.
+ *
+ * @args: An array of strings containing the command
+ * and its arguments.
  */
 void execute_command(char **args) {
     pid_t pid;
@@ -12,8 +39,10 @@ void execute_command(char **args) {
     char error_message[256];
 
     if (args == NULL || args[0] == NULL) {
-        
         return;
+    }
+    if (execute_builtin(args)) {
+        return; 
     }
 
     pid = fork();
@@ -23,7 +52,7 @@ void execute_command(char **args) {
     }
 
     if (pid == 0) {
-        
+       
         execve(args[0], args, environ);
 
         
@@ -32,7 +61,6 @@ void execute_command(char **args) {
         write(STDERR_FILENO, error_message, strlen(error_message));
         exit(EXIT_FAILURE);
     } else {
-        
         wait(&status);
     }
 }
