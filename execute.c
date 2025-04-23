a8deaad5bb276b22bf7646d13316c479a7aff7f7
Your Name <bezom@hotmail.fr>
2025-04-23 15:15:32 +0200

    modification du fichier execute

--- execute.c
+++ execute.c
@@ -8,45 +8,37 @@ void execute_command(char **args, char *program_name)
 {
     pid_t pid;
     int status;
-    char *command_path = NULL;
-    struct stat st;
+    char *command_path;
 
     if (args[0][0] == '/' || args[0][0] == '.')
     {
-        if (stat(args[0], &st) == 0)
+        struct stat st;
+        
+        if (stat(args[0], &st) == 0 && S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
         {
-            if (S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
-            {
-                command_path = _strdup(args[0]);
-            }
-            else
-            {
-                errno = EACCES;
-                perror(args[0]);
-                return;
-            }
+            command_path = _strdup(args[0]);
         }
         else
         {
-            perror(args[0]);
-            return;
+            command_path = NULL;
         }
     }
     else
     {
         command_path = get_full_path(args[0]);
-        if (!command_path)
-        {
-            write(STDERR_FILENO, program_name, _strlen(program_name));
-            write(STDERR_FILENO, ": command not found\n", 20);
-            return;
-        }
+    }
+
+    if (!command_path)
+    {
+        fprintf(stderr, "%s: %s: not found\n", program_name, args[0]);
+        return;
     }
 
     pid = fork();
     if (pid == 0)
     {
         execve(command_path, args, environ);
+
         perror(command_path);
         exit(EXIT_FAILURE);
     }
