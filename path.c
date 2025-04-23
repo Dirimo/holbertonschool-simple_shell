246d1a69c25f5bc56afb4dd2a00e4a0e20c1287d
Your Name <bezom@hotmail.fr>
2025-04-23 16:10:35 +0200

    modification fichier path.c

--- path.c
+++ path.c
@@ -4,48 +4,64 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
-/* get_full_path - find the full path of a command using the PATH variable */
+extern char **environ;
+
 char *get_full_path(char *command)
 {
-	char *path = NULL, *path_copy = NULL, *dir, *full_path;
-	struct stat st;
+    char *path = NULL, *path_copy = NULL, *dir, *full_path;
+    struct stat st;
+    int i;
 
-	if (strchr(command, '/'))
-		return NULL;
+    
+    for (i = 0; environ[i]; i++)
+    {
+        if (strncmp(environ[i], "PATH=", 5) == 0)
+        {
+            path = environ[i] + 5;
+            break;
+        }
+    }
 
-	path = getenv("PATH");
-	if (!path)
-		path = "/usr/local/bin:/usr/bin:/bin";
+    if (!path)
+        return NULL;
 
-	path_copy = _strdup(path);
-	if (!path_copy)
-		return NULL;
+    path_copy = strdup(path);
+    if (!path_copy)
+        return NULL;
 
-	dir = strtok(path_copy, ":");
-	while (dir != NULL)
-	{
-		size_t len = _strlen(dir) + _strlen(command) + 2;
-		full_path = malloc(len);
-		if (!full_path)
-		{
-			free(path_copy);
-			return NULL;
-		}
+    dir = strtok(path_copy, ":");
+    while (dir != NULL)
+    {
+        size_t len = strlen(dir) + strlen(command) + 2;
+        full_path = malloc(len);
+        if (!full_path)
+        {
+            free(path_copy);
+            return NULL;
+        }
 
-		snprintf(full_path, len, "%s/%s", dir, command);
+        snprintf(full_path, len, "%s/%s", dir, command);
 
-		if (stat(full_path, &st) == 0 &&
-		    S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
-		{
-			free(path_copy);
-			return full_path;
-		}
+        if (stat(full_path, &st) == 0)
+        {
+            if (S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
+            {
+                free(path_copy);
+                return full_path;
+            }
+            errno = EACCES;
+            free(full_path);
+        }
+        else
+        {
+            free(full_path);
+        }
+        dir = strtok(NULL, ":");
+    }
 
-		free(full_path);
-		dir = strtok(NULL, ":");
-	}
-
-	free(path_copy);
-	return NULL;
+    free(path_copy);
+    fprintf(stderr, "%s: command not found\n", command);
+    return NULL;
 }
