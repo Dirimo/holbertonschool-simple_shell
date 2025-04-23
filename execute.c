2bd4d9ee63d8950e8f2ca5cef9ed21fc3450796c
Your Name <bezom@hotmail.fr>
2025-04-23 15:00:59 +0200

    modification fichier path.c

--- execute.c
+++ execute.c
@@ -8,38 +8,45 @@ void execute_command(char **args, char *program_name)
 {
     pid_t pid;
     int status;
-    char *command_path;
+    char *command_path = NULL;
+    struct stat st;
 
     if (args[0][0] == '/' || args[0][0] == '.')
     {
-        struct stat st;
-        
-        if (stat(args[0], &st) == 0 && S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
+        if (stat(args[0], &st) == 0)
         {
-            command_path = _strdup(args[0]);
+            if (S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
+            {
+                command_path = _strdup(args[0]);
+            }
+            else
+            {
+                errno = EACCES;
+                perror(args[0]);
+                return;
+            }
         }
         else
         {
-            command_path = NULL;
+            perror(args[0]);
+            return;
         }
     }
     else
     {
         command_path = get_full_path(args[0]);
-    }
-
-    if (!command_path)
-    {
-        write(STDERR_FILENO, program_name, _strlen(program_name));
-        write(STDERR_FILENO, ": command not found\n", 20);
-        return;
+        if (!command_path)
+        {
+            write(STDERR_FILENO, program_name, _strlen(program_name));
+            write(STDERR_FILENO, ": command not found\n", 20);
+            return;
+        }
     }
 
     pid = fork();
     if (pid == 0)
     {
         execve(command_path, args, environ);
-
         perror(command_path);
         exit(EXIT_FAILURE);
     }
@@ -55,4 +62,3 @@ void execute_command(char **args, char *program_name)
 
     free(command_path);
 }
-
--- path.c
+++ path.c
@@ -2,65 +2,22 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <errno.h>
 
 char *get_full_path(char *command)
 {
-	char *path, *path_copy, *dir, *full_path;
-	struct stat st;
-
-
-	path = getenv("PATH");
-	if (!path)
-		return NULL;
-
-	path_copy = _strdup(path);
-	if (!path_copy)
-		return NULL;
-
-	dir = strtok(path_copy, ":");
-	while (dir != NULL)
-	{
-		full_path = malloc(_strlen(dir) + _strlen(command) + 2);
-		if (!full_path)
-		{
-			free(path_copy);
-			return NULL;
-		}
-
-		_strcpy(full_path, dir);
-		_strcat(full_path, "/");
-		_strcat(full_path, command);
-
-		if (stat(full_path, &st) == 0 && S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
-		{
-			free(path_copy);
-			return full_path;
-		}
-
-		free(full_path);
-		dir = strtok(NULL, ":");
-	}
-
-	free(path_copy);
-	return NULL;
-
-    if (strchr(command, '/')) {
-        if (stat(command, &st) == 0) {
-            if (S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR)) {
-                return _strdup(command);
-            }
-            errno = EACCES;
-        }
-        perror(command);
-        return NULL;
-    }
+    char *path = NULL, *path_copy = NULL, *dir, *full_path;
+    struct stat st;
 
     path = getenv("PATH");
-    if (!path) path = "/usr/local/bin:/usr/bin:/bin";
+    if (!path)
+        return NULL;
 
     path_copy = _strdup(path);
-    if (!path_copy) return NULL;
+    if (!path_copy)
+        return NULL;
 
     dir = strtok(path_copy, ":");
     while (dir != NULL)
