0943d63b5c8309b1ec09a93509760182265e8725
Dirimo <dirimoiriarte@gmail.com>
2025-04-23 10:53:12 +0200

    Final correction of my path

--- path.c
+++ path.c
@@ -1,39 +1,36 @@
 #include "shell.h"
 #include <sys/stat.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h> // For NULL
 
-char *get_full_path(char *command) {
+char *get_full_path(char *command)
+{
 	char *path, *path_copy, *dir, *full_path;
 	struct stat st;
 
 	path = getenv("PATH");
-	if (!path) {
+	if (!path)
 		return NULL;
-	}
 
-	path_copy = strdup(path);
-	if (!path_copy) {
-		return NULL; // Handle strdup failure
-	}
+	path_copy = _strdup(path);
+	if (!path_copy)
+		return NULL;
 
 	dir = strtok(path_copy, ":");
-	while (dir != NULL) {
-		size_t full_path_len = strlen(dir) + strlen(command) + 2; // +1 for '/', +1 for null terminator
-		full_path = malloc(full_path_len);
-
-		if (!full_path) {
+	while (dir != NULL)
+	{
+		full_path = malloc(_strlen(dir) + _strlen(command) + 2);
+		if (!full_path)
+		{
 			free(path_copy);
 			return NULL;
 		}
 
-		strcpy(full_path, dir);
-		strcat(full_path, "/");
-		strcat(full_path, command);
+		_strcpy(full_path, dir);
+		_strcat(full_path, "/");
+		_strcat(full_path, command);
 
-		if (stat(full_path, &st) == 0 && S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR)) {
+		if (stat(full_path, &st) == 0 && S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
+		{
 			free(path_copy);
 			return full_path;
 		}
