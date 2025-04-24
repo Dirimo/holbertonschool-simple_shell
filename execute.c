c9e71cc997ae5f3b5714e2c8d08a32d818a9b78e
Dirimo <dirimoiriarte@gmail.com>
2025-04-24 11:45:56 +0200

    correction of my file execute because a merge problem

--- execute.c
+++ execute.c
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-<<<<<<< HEAD
 
 // Assuming get_full_path is defined in path.c and declared in shell.h
 extern char *get_full_path(char *command);
@@ -39,58 +38,4 @@ void execute_command(char **args, char *program_name) {
 		wait(&status);
 		free(full_path); // Free the allocated path after the child process is done
 	}
-=======
-#include <errno.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-/**
- * execute_command - exécute une commande dans un nouveau processus
- * @args: tableau d'arguments (commande + arguments)
- * @program_name: nom du programme principal (argv[0]) pour les messages d'erreur
- */
-void execute_command(char **args, char *program_name)
-{
-	pid_t pid;
-	int status;
-	char *command_path = NULL;
-	struct stat st;
-
-	if (strchr(args[0], '/'))
-	{
-		if (stat(args[0], &st) == 0 && S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
-			command_path = _strdup(args[0]);
-	}
-	else
-	{
-		command_path = get_full_path(args[0]);
-	}
-
-	if (!command_path)
-	{
-		fprintf(stderr, "%s: %s: not found\n", program_name, args[0]);
-		return;
-	}
-
-	pid = fork();
-	if (pid == 0)
-	{
-		if (execve(command_path, args, environ) == -1)
-		{
-			perror(command_path);
-			free(command_path);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else if (pid > 0)
-	{
-		wait(&status);
-	}
-	else
-	{
-		perror("fork");
-	}
-
-	free(command_path);
->>>>>>> refs/remotes/origin/main
 }
