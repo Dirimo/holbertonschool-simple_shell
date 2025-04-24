bcd202c1fe90fddf3bed88dba2599530462a6a52
Dirimo <dirimoiriarte@gmail.com>
2025-04-24 11:57:46 +0200

    correction of my file execute

--- execute.c
+++ execute.c
@@ -1,6 +1,8 @@
+
 #include "shell.h"
-#include <sys/stat.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
 
@@ -12,47 +14,36 @@ void execute_command(char **args, char *program_name)
 
 	if (args[0][0] == '/' || args[0][0] == '.')
 	{
-		struct stat st;
-
-		if (stat(args[0], &st) == 0 && S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
-		{
-			command_path = _strdup(args[0]);
-		}
-		else
-		{
-			command_path = NULL;
-		}
-
-		else
-		{
-			command_path = get_full_path(args[0]);
-		}
-
-		if (!command_path)
-		{
-			write(STDERR_FILENO, program_name, _strlen(program_name));
-			write(STDERR_FILENO, ": command not found\n", 20);
-
-			return;
-		}
-
-		pid = fork();
-		if (pid == 0)
-		{
-			execve(command_path, args, environ);
-
-			perror(command_path);
-			exit(EXIT_FAILURE);
-		}
-		else if (pid > 0)
-		{
-			wait(&status);
-		}
-		else
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-
-		free(command_path);
+		command_path = _strdup(args[0]);
 	}
+	else
+	{
+		command_path = get_full_path(args[0]);
+	}
+
+	if (!command_path)
+	{
+		write(STDERR_FILENO, program_name, _strlen(program_name));
+		write(STDERR_FILENO, ": command not found\n", 20);
+		return;
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(command_path, args, environ);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid > 0)
+	{
+		wait(&status);
+	}
+	else
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+
+	free(command_path);
+}
