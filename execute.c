f35581d1d2b5752b8ea76e88a0123e86e9130d16
Dirimo <dirimoiriarte@gmail.com>
2025-04-24 10:47:39 +0200

    Correction of script execute

--- execute.c
+++ execute.c
@@ -1,58 +1,41 @@
 #include "shell.h"
-#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <errno.h>
 
-void execute_command(char **args, char *program_name)
-{
-    pid_t pid;
-    int status;
-    char *command_path;
+// Assuming get_full_path is defined in path.c and declared in shell.h
+extern char *get_full_path(char *command);
 
-    if (args[0][0] == '/' || args[0][0] == '.')
-    {
-        struct stat st;
-        
-        if (stat(args[0], &st) == 0 && S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
-        {
-            command_path = _strdup(args[0]);
-        }
-        else
-        {
-            command_path = NULL;
-        }
-    }
-    else
-    {
-        command_path = get_full_path(args[0]);
-    }
+void execute_command(char **args, char *program_name) {
+	char *command = args[0];
+	char *full_path = get_full_path(command);
 
-    if (!command_path)
-    {
-        write(STDERR_FILENO, program_name, _strlen(program_name));
-        write(STDERR_FILENO, ": command not found\n", 20);
-        return;
-    }
+	if (full_path == NULL) {
+		fprintf(stderr, "%s: %s: command not found\n", program_name, command);
+		return; // Do NOT fork
+	}
 
-    pid = fork();
-    if (pid == 0)
-    {
-        execve(command_path, args, environ);
+	pid_t pid = fork();
 
-        perror(command_path);
-        exit(EXIT_FAILURE);
-    }
-    else if (pid > 0)
-    {
-        wait(&status);
-    }
-    else
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
+	if (pid == -1) {
+		perror("fork");
+		free(full_path);
+		return;
+	}
 
-    free(command_path);
+	if (pid == 0) {
+		// Child process
+		if (execve(full_path, args, environ) == -1) {
+			perror("execve");
+			free(full_path); // Free before exiting in child
+			exit(EXIT_FAILURE); // Important: exit in the child process on execve failure
+		}
+	} else {
+		// Parent process
+		int status;
+		wait(&status);
+		free(full_path); // Free the allocated path after the child process is done
+	}
 }
-
